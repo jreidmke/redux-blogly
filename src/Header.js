c7f6048c457b4d8228bdddb8962f4f08272e64f3
unknown <jreidmke@gmail.com>
2020-12-14 11:59:13 -0600

    Header comp draft

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,15 @@
+import {NavLink} from 'react-router-dom';
+import './NavBar.css';
+
 const Header = () => {
     return(
         <div>
             <h1>Blogger</h1>
             <h4>Write your inner most thoughts</h4>
-            
+            <NavLink to="/home">The Blog</NavLink>
+            <NavLink to="/new">Create a New Post</NavLink>
         </div>
     )
 }
+
+export default Header;
