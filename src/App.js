64f06b9d5d4e63872386f9d3eedcfea8b06bf437
unknown <jreidmke@gmail.com>
2020-12-14 12:37:28 -0600

    imported Post components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+//Components
 import Header from './Header';
 import Comment from './Comment';
 import CommentForm from './CommentForm';
-import {useState} from 'react';
+import Post from './Post';
+import PostForm from './PostForm';
 
 function App() {
   const [comments, setComments] = useState([]);
@@ -16,6 +20,7 @@ function App() {
     <div className="App">
       <Header/>
       <CommentForm addComment={createComment}/>
+      <Post title='Pizza' description='Pizza is so good!' post='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'/>
       {commentsRender}
     </div>
   );
