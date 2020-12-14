8d72780fbd0f5a2592cf726be98a1de1157f4cd9
unknown <jreidmke@gmail.com>
2020-12-14 12:50:13 -0600

    imported Post components

--- src/App.js
+++ src/App.js
@@ -11,17 +11,18 @@ import PostForm from './PostForm';
 
 function App() {
   const [comments, setComments] = useState([]);
+  const [posts, setPosts] = useState([]);
 
   const createComment = (newComment) => setComments([...comments, newComment]);
+
+  const addPost = (newPost) => setPosts([...posts, newPost]);
 
   const commentsRender = comments.map(c => <Comment comment={c.comment} key={c.id}/>)
 
   return (
     <div className="App">
       <Header/>
-      <CommentForm addComment={createComment}/>
-      <Post title='Pizza' description='Pizza is so good!' post='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'/>
-      {commentsRender}
+      <PostForm createPost={addPost}/>
     </div>
   );
 }
