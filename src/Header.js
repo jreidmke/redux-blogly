9960fb92770861c94cc84a2d4e300f1fc02be49d
unknown <jreidmke@gmail.com>
2020-12-14 12:03:48 -0600

    styling

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,12 @@
 import {NavLink} from 'react-router-dom';
-import './NavBar.css';
+import './Header.css';
 
 const Header = () => {
     return(
         <div>
             <h1>Blogger</h1>
             <h4>Write your inner most thoughts</h4>
-            <NavLink to="/home">The Blog</NavLink>
+            <NavLink to="/">The Blog</NavLink>
             <NavLink to="/new">Create a New Post</NavLink>
         </div>
     )
