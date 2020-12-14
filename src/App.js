4259796c4079d517a72725edcee8d02e49bf5f7b
unknown <jreidmke@gmail.com>
2020-12-14 12:27:38 -0600

    imported comment and form comps

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Comment from './Comment';
+import CommentForm from './CommentForm';
+import {useState} from 'react';
 
 function App() {
+  const [comments, setComments] = useState([]);
+
+  const createComment = (newComment) => setComments([...comments, newComment]);
+
+  const commentsRender = comments.map(c => <Comment comment={c.comment}/>)
+
   return (
     <div className="App">
       <Header/>
+      <CommentForm addComment={createComment}/>
+      {commentsRender}
     </div>
   );
 }
