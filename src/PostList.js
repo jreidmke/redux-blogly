30ecfba6aae16ee3f33c518094b53abbbb5f03bf
unknown <jreidmke@gmail.com>
2020-12-14 13:30:38 -0600

    formatted post list to allow ids

--- src/PostList.js
+++ src/PostList.js
@@ -1,16 +1,17 @@
 import {useState} from 'react';
 import {Link} from 'react-router-dom';
 import Post from './Post';
+import data from './dummyPosts.json';
 
 const PostList = () => {
     const [posts, setPosts] = useState([]);
 
     const addPost = (newPost) => setPosts([...posts, newPost]);
-    const postsRender = posts.map(p => <Post title={p.title} description={p.description} post={p.post} key={p.id}/>);
+    const postsRender = data.map(p => <Post title={p.title} description={p.description} post={p.post} key={p.id}/>);
 
     return(
         <ul>
-            {posts.map(p => <li><Link to={`/${p.id}`}><Post title={p.title} key={p.id}/></Link></li>)}
+            {data.map(p => <li><Link to={`/${p.id}`}><Post title={p.title} key={p.id} id={p.id}/></Link></li>)}
         </ul>
     )
 }
