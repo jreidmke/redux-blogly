8433685b1db3f6eea9ffb78fe1969aa7119252b2
unknown <jreidmke@gmail.com>
2020-12-14 12:07:19 -0600

    styling

--- src/Header.js
+++ src/Header.js
@@ -3,12 +3,12 @@ import './Header.css';
 
 const Header = () => {
     return(
-        <div>
+        <nav>
             <h1>Blogger</h1>
             <h4>Write your inner most thoughts</h4>
             <NavLink to="/">The Blog</NavLink>
             <NavLink to="/new">Create a New Post</NavLink>
-        </div>
+        </nav>
     )
 }
 
