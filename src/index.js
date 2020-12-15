e467f1b78b57e361a7921cfb9ea18636a12b2228
unknown <jreidmke@gmail.com>
2020-12-15 09:22:53 -0600

    corrected rootReducer import bug

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 import { createStore } from 'redux';
-import rootReducer from './reducers/rootReducer'
+import rootReducer from './reducers/rootReducer';
 import { Provider } from 'react-redux';
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
