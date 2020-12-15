e59e4e9c9671fc453c04a4d3f01257dd4d2c5d6e
unknown <jreidmke@gmail.com>
2020-12-15 13:26:41 -0600

    installed thunk and refactored store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/rootReducer';
 import { Provider } from 'react-redux';
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from "redux-thunk";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
